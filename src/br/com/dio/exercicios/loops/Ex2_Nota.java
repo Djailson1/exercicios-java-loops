be7e6b83ab9acbd0357bf5b17747d1cac7ed4eea
Djailson1 <djailson_silva1@outlook.com>
2022-02-17 06:39:49 -0300

    exercicio java loops

---
+++ src/br/com/dio/exercicios/loops/Ex2_Nota.java
@@ -0,0 +1,28 @@
+package br.com.dio.exercicios.loops;
+
+import java.util.Scanner;
+
+/*Faça um programa que peça um nota, entre  0 e 10.
+* Mostre uma mensagem caso o valor seja inválido
+* e continue pedindo
+* até que o usuário informe um valor válido.
+* */
+
+public class Ex2_Nota {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Nota de 0 a 10: ");
+        nota = scan.nextInt();
+
+        while (nota < 0 | nota > 10) {
+            System.out.println("Nota inválida! Digite novamente: ");
+            nota = scan.nextInt();
+
+        }
+
+
+    }
+}
---
+++ src/br/com/dio/exercicios/loops/Main.java
@@ -0,0 +1,7 @@
+package br.com.dio.exercicios.loops;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
